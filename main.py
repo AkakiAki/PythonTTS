648bc01f3af07c277ff90e1e57995bb70118da05
Akaki <47449677+AkakiAki@users.noreply.github.com>
2024-06-09 21:25:52 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import pypandoc
+# uncomment line 3-4 if pypandoc is not found
+# from pypandoc.pandoc_download import download_pandoc
+# download_pandoc()
+
+import pyttsx3
+# in case of libspeak.so.1 error install: pip install pyaudio or easpeak or libespeak-dev
+# these are for tts audio styles
+
+# tts = text to speech
+# tts initialization
+engine = pyttsx3.init()
+newVoiceRate = 110
+engine.setProperty('rate', newVoiceRate)
+
+# tts from user input
+speak = input("Enter text to speak: ")
+engine.say(speak)
+engine.runAndWait()
+
+# export docx or any file into text file to read in next step
+document = 'java.docx'
+output = pypandoc.convert_file(document, 'plain', outputfile="converted_text.txt")
+assert output == ""
+
+# Read from txt file
+with open('converted_text.txt', 'r') as f:
+    textfile_content = f.readlines()
+    textfile_content = [x.replace('\n', '') for x in textfile_content]
+    # print(textfile_content)
+
+engine.say(textfile_content)
+engine.runAndWait()
